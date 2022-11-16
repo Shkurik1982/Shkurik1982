84813bf24ea977f14a71f125da40cf75eb14e03e
Shkurik1982 <112969806+Shkurik1982@users.noreply.github.com>
2022-11-16 10:43:41 +0300

    Update hw.py

--- python/hw.py
+++ python/hw.py
@@ -1,24 +1,22 @@
 import random
-txt=txt1=''
-a=1
-b=random.randint(1,10)
-print(b)
+a='new'
+
 while True:
-    if ( (a>=1 and a<=10) and (str(a)!="exit") and (str(a)!="new")) :
-        a=input(txt+'Введите число от 1 до 10 or exit\n'+txt1+"или exit для выхода, или new для нового раунда\n")
-        if a=="exit":continue
-        print("a=",a)
-        print("b=",b)
-        if int(a)==b:
-            txt='Вы выиграли. '
-        elif  int(a) > b:
-            txt="Много. "
-        else:
-            txt="Мало. "
-    elif (a=="exit"):
-        break
-    elif a=="new":
-        b=random.randint(1,10)
-        print(b)
-        a=b+1
-    else: continue
+    txt=''
+    try: 
+        if int(a)>=1 and int(a)<=10:
+            #b=random.randint(1,10)
+            if int(a)==b:
+                txt='Вы выиграли.\nНачинаем новый раунд\n'
+                b=random.randint(1,10)
+            elif  int(a) > b:
+                txt="Много.\n"
+            else:
+                txt="Мало.\n"
+    except:
+        if a=="exit":
+            break
+        if a=="new":
+            b=random.randint(1,10)
+            txt="Начинаем новый раунд\n"
+    a=input(txt+'Введите число от 1 до 10\nexit для выхода, или new для нового раунда\n')
